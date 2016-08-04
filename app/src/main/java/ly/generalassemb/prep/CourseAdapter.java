d0f0df3047fac13be23081c57c4c0d59e4f83645
Aaron Fields <afields410@gmail.com>
2016-08-04 14:40:19 -0500

    Course Adapter

--- app/src/main/java/ly/generalassemb/prep/CourseAdapter.java
+++ app/src/main/java/ly/generalassemb/prep/CourseAdapter.java
@@ -1,33 +1,80 @@
 package ly.generalassemb.prep;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by aaronfields on 8/2/16.
  */
-public class CourseAdapter extends RecyclerView.Adapter{
+public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.courseViewHolder>{
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference subjectRef = database.getReference("subjects");
+    DatabaseReference courseRef = database.getReference("courses");
+    LayoutInflater inflater;
+    Context context;
+    List<String> courseList;
+    List<String> mKeys;
 
+    public CourseAdapter(List<String> courseList, List<String> keys, Context context){
+        this.courseList = courseList;
+        this.context = context;
+        this.mKeys = keys;
+    }
 
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public courseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = inflater.from(parent.getContext()).inflate(R.layout.course_item, parent, false);
+        courseViewHolder holder = new courseViewHolder(v, context, courseList);
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(courseViewHolder holder, int position) {
+        String course = courseList.get(position);
+        holder.mCourse.setText(courseList.get(position));
     }
 
     @Override
     public int getItemCount() {
         return 0;
+    }
+
+    public class courseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView mCourse;
+        List<String> courseList = new ArrayList<>();
+        Context context;
+
+        public courseViewHolder(View itemView, Context context, List<String> course) {
+            super(itemView);
+            this.courseList = course;
+            this.context = context;
+
+            itemView.setOnClickListener(this);
+            mCourse = (TextView) itemView.findViewById(R.id.course);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+
+            String course = courseList.get(position);
+            Intent intent = new Intent(this.context, MapsActivity.class);
+            intent.putExtra("course", course);
+            this.context.startActivity(intent);
+
+        }
     }
 }
